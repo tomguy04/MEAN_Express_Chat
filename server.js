8faa6838ee633debb80a56e7d2cf4189dd34b877
Glen Goulter <glenrg@gmail.com>
2018-03-30 10:13:38 -0700

    v2, getting message objects to all users with array of message objects.

--- server.js
+++ server.js
@@ -13,14 +13,6 @@ var bodyParser = require('body-parser');
 // use it!
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//use session
-// app.use(session({
-//     secret: 'codingdojorocks', // string for encryption
-//     resave:true,
-//     saveUninitialized: true,
-//     dateRequested = new Date()
-// }));
-
 //require session
 var session = require('express-session');
 //use session
@@ -40,37 +32,40 @@ var server = app.listen(8000, function() {
 
 
 //user object
-class User {
-    constructor(name, id) {
+class userMessage {
+    constructor(name, message) {
             this.name = name;
-            this.id = id;
+            this.message = message;
         }
     }
 
 //the io object which we are going to use to control our sockets.
 var io = require('socket.io').listen(server);
-//users =[];
+var allMessages =[];
 //we are going to set up the connection event. 
 //Remember the order! Server and our port listener come first, the 'io' variable and require socket statement second, and last we'll have the io.sockets.on line as seen in the below snippet:
 
 io.sockets.on('connection', function (socket, request, response) { //when a client connects, this code runs!
     console.log("Client/socket is connected!");
     console.log("Client/socket id is: ", socket.id);
-    socket.on( "got_a_new_user", function (request,response){
-        console.log(request.name);
-        socket.broadcast.emit( 'new_user', {new_user_name: request.name});
-        //var users = users || [];
-        //req.session.user_name = req.name;
+    socket.on( "got_a_new_user", function (data){
+        console.log(data.name);
+        //socket.broadcast.emit( 'new_user', {new_user_name: request.name});
 
         //store the user info in a user object
-        const myuser = new User(request.name, socket.id);
-        console.log(`Create user -> ${myuser.name} ${myuser.id}`);
-        //console.log(request.users);
+        //const myuser = new User(request.name, socket.id);
+        console.log(`Created user -> ${data.name} ${socket.id}`);
+  ;
         //return the user name and id back to the client.
-        socket.emit('this_user',{response: myuser});
+        socket.emit('userdata',{name: data.name, sid: socket.id});
         })
 
-    socket.on('posting_form', function (request,response){
-        console.log(`${request.name} ${request.message}`);
+    socket.on('message', function (data, res){
+        //console.log(`Their name and message ${data.name} ${data.message}`);
+        newMessage = new userMessage(data.name, data.message);
+        allMessages.push(newMessage);
+        //socket.emit('messageBack',{messages: data.name, message: data.message, sid: socket.id});
+        socket.emit('messageBack',{messages: allMessages});
+        //console.log(data.name);
     })
 })
