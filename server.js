ed830f1863949d25064984bc2e462ce7b40127cf
Glen Goulter <glenrg@gmail.com>
2018-03-30 12:59:13 -0700

    chat is working, need to add event for when I user closes their window, remove them from the chat (don't show their name anymore in the list of logged in users).

--- server.js
+++ server.js
@@ -32,16 +32,27 @@ var server = app.listen(8000, function() {
 
 
 //user object
-class userMessage {
-    constructor(name, message) {
-            this.name = name;
+class Message {
+    //constructor(name, message) {
+    constructor(user, message) {
+            //this.name = name;
+            this.user = user;
             this.message = message;
+            this.timeStamp = new Date();
+        }
+    }
+
+class User {
+    constructor(name, id) {
+            this.name = name;
+            this.id = id;
         }
     }
 
 //the io object which we are going to use to control our sockets.
 var io = require('socket.io').listen(server);
 var allMessages =[];
+var allUsers = [];
 //we are going to set up the connection event. 
 //Remember the order! Server and our port listener come first, the 'io' variable and require socket statement second, and last we'll have the io.sockets.on line as seen in the below snippet:
 
@@ -51,21 +62,39 @@ io.sockets.on('connection', function (socket, request, response) { //when a clie
     socket.on( "got_a_new_user", function (data){
         console.log(data.name);
         //socket.broadcast.emit( 'new_user', {new_user_name: request.name});
-
+        const user = new User(data.name, socket.id);
+        allUsers.push(user);
         //store the user info in a user object
         //const myuser = new User(request.name, socket.id);
-        console.log(`Created user -> ${data.name} ${socket.id}`);
-  ;
-        //return the user name and id back to the client.
-        socket.emit('userdata',{name: data.name, sid: socket.id});
-        })
+        console.log(`Created user -> ${user.name} ${user.id}`);
+        //console.log(io.broadcast);
+        //console.log(socket.broadcast);
+        //return the user name and id back to everyone but the new user.
+        socket.broadcast.emit('userdata', user);
 
-    socket.on('message', function (data, res){
+        //new user logs in sees all users
+        socket.emit('allUsers', allUsers);
+        //new user gets their name and id back within the user object.
+        socket.emit('userDetails',user);
+        //new user gets the message history.
+        socket.emit('allMessages', allMessages);
+    })
+
+
+
+    socket.on('message', function (data){
+        const user = allUsers.find(possibleUser => possibleUser.id === data.id) //if the user id in the array matches the id passed on the event, do something.
         //console.log(`Their name and message ${data.name} ${data.message}`);
-        newMessage = new userMessage(data.name, data.message);
+        console.log(`Their name and message ${user.name} ${data.message}`);
+        const newMessage = new Message(user, data.message);
         allMessages.push(newMessage);
         //socket.emit('messageBack',{messages: data.name, message: data.message, sid: socket.id});
-        socket.emit('messageBack',{messages: allMessages});
+        
+        //socket.emit('messageBack',{messages: allMessages});
+        //send back only new messages to everyone.
+        io.emit('messageBack', newMessage);
         //console.log(data.name);
     })
+    //a client closes their window:
+    //socket.on
 })
